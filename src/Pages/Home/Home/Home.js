6da50539c99159c385cc8c13a6451a3aeea25c8c
BEBACK100 <beback_bain@yahoo.com>
2022-11-24 01:54:59 +0600

    navbar commit

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Advertis from '../Advartise/Advertis';
+import Slider from '../Slider/Slider';
+
+const Home = () => {
+    return (
+        <div>
+            <Slider></Slider>
+            <Advertis></Advertis>
+        </div>
+    );
+};
+
+export default Home;
--- src/Pages/Home/Navbar/Navbar.js
+++ src/Pages/Home/Navbar/Navbar.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import img from './logo2.jpg'
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>This is navbar</h1>
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a>Item 1</a></li>
+                        <li tabIndex={0}>
+                            <a className="justify-between">
+                                Parent
+                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
+                            </a>
+                            <ul className="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </li>
+                        <li><a>Item 3</a></li>
+                    </ul>
+                </div>
+                <a className="btn btn-ghost normal-case text-xl">Laptop-Bazar(Second-hand)</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal gap-3">
+                    <button className="btn btn-outline  btn-success">Home</button>
+                    <button className="btn btn-outline btn-success">Blog</button>
+                    <button className="btn btn-outline btn-success">Login</button>
+                    <button className="btn btn-outline btn-success">Button</button>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <img className='w-20' src={img} alt="" />
+            </div>
         </div>
     );
 };
